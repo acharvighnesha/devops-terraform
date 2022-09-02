0454ce3e57dee946ab8c4d3e9e5f2a0bbe903066
acharvighnesha <vighnesha.achar@swiggy.in>
2022-09-02 13:04:04 +0530

    added sg & ec2 module

--- terraform-with-modules/vpc.tf
+++ terraform-with-modules/vpc.tf
@@ -17,3 +17,41 @@ module "vpc" {
     Batch = "c34"
   }
 }
+
+module "vighnesha_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "ssh-sg"
+  description = "Security group for ssh into three tier architecture"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "ec2-instance"
+
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = [module.vighnesha_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
